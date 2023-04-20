97bc959200ebde7905d32847fc1d0c9acb249cb9
WolfSpiderman <wolfspiderman@yahoo.com>
2023-04-20 18:34:11 -0500

    added comments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+// Import the router for notes
 const notesRouter = require('./notes');
 
 const app = express();
 
+// Use the notes router at the /notes endpoint
 app.use('/notes', notesRouter);
 
 module.exports = app;
